c3d125937c3045357cd992e945ab6287209b1f54
Onion4070 <naonaosub.0704@gmail.com>
2026-01-18 18:08:34 +0900

    API経由でDiscord通知

---
+++ notify_test.py
@@ -0,0 +1,25 @@
+import os
+import requests as req
+import json
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+TOKEN      = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
+
+def notify():
+    msg = "Hello World"
+
+    header = {
+        'authorization':f'Bot {TOKEN}', 
+        'content-type':'application/json'
+    }
+
+    payload = {'content':msg}
+    payload = json.dumps(payload)
+
+    url = f'https://discordapp.com/api/channels/{CHANNEL_ID}/messages'
+    req.post(url, data=payload, headers=header)
+
+if __name__ == '__main__':
+    notify()
