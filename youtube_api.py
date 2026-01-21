2b0a364b4ab943a5c09716d4b7ef33d1bc880c1a
Onion4070 <naonaosub.0704@gmail.com>
2026-01-21 21:21:37 +0900

    YoutubeAPIを非同期処理に変更

--- youtube_api.py
+++ youtube_api.py
@@ -1,27 +1,41 @@
 import os
 from dotenv import load_dotenv
-from datetime import datetime
-import requests as req
+import aiohttp
+import asyncio
 
 
 load_dotenv('.env')
 KEY = os.getenv('YOUTUBE_API_KEY')
+VIDEO_ID = os.getenv('TEST_VIDEO_ID')
 
 
-def get_json(video_id: str):
-    URL = f'https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics,snippet&id={video_id}&key={KEY}'
-    res_json = req.get(URL).json()
-    return res_json
+async def get_json(video_id: str):
+    url = f'https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics,snippet&id={video_id}&key={KEY}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                return await response.json()
+            else:
+                return f'error: status code {response.status}'
 
 
-def get_live_status(video_id: str):
-    res_json = get_json(video_id)
+async def get_live_status(video_id: str):
+    res_json = await get_json(video_id)
+    print(res_json)
+    items = res_json['items']
+    item  = items[0]
     live_status = res_json['items'][0]['snippet']['liveBroadcastContent']
     # live     -> 'live', 
     # upcoming -> 'upcoming'
     # else     -> 'none'
-    return live_status 
+    return live_status
 
+
+async def main():
+    res_json = await get_json(VIDEO_ID)
+    status = await get_live_status(VIDEO_ID)
+    #print(res_json)
+    #print(status)
 
 if __name__ == '__main__':
-    print(get_live_status(os.getenv('TEST_VIDEO_ID')))
+    asyncio.run(main())
